a97ffedc0319d9dc138e0e1e15464dc692179b92
davem <dave.mike.ball@gmail.com>
2019-12-02 12:32:35 +0000

    commit various data preprocessing examples, rescaling, standardisation,normilisation and binarization

---
+++ normalised_example.py
@@ -0,0 +1,23 @@
+# data rescaling for (0 to 1)
+from pathlib import Path
+from numpy import set_printoptions
+from sklearn.preprocessing import Normalizer
+from pandas import read_csv
+
+# load  data  file
+data_folder = Path("data/")
+file_to_open = data_folder / "pima-indians-diabetes.csv"
+
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataFrame = read_csv(file_to_open, names=names)
+array = dataFrame.values
+
+# scale  data
+x = array[:, 0:8]
+y = array[:, 8]
+scaler = Normalizer().fit(x)
+rescaledX = scaler.fit_transform(x)
+
+# summarise transformed data
+set_printoptions(precision=3)
+print(rescaledX[0:5, :])
