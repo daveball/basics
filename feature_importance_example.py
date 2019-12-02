fb257c2b62c28ebfe545ff78b4f8357d451944f0
davem <dave.mike.ball@gmail.com>
2019-12-02 14:23:06 +0000

    commit various data feature detection methods univeriate, recurcive, pca, and feature importance

---
+++ feature_importance_example.py
@@ -0,0 +1,24 @@
+# feature importance
+from pathlib import Path
+from numpy import set_printoptions
+from sklearn.ensemble import ExtraTreesClassifier
+from pandas import read_csv
+
+# load  data  file
+data_folder = Path("data/")
+file_to_open = data_folder / "pima-indians-diabetes.csv"
+
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataFrame = read_csv(file_to_open, names=names)
+array = dataFrame.values
+
+# scale  data
+x = array[:, 0:8]
+y = array[:, 8]
+
+model = ExtraTreesClassifier(n_estimators=100)
+model.fit(x, y)
+
+print(model.feature_importances_)
+
+
